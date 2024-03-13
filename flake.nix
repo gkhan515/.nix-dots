84fbd6534166bc795a2b043ef652e0a89801ce5c
Gibron Khan <khangibron@gmail.com>
2024-03-13 16:39:10 -0400

    add specialArgs

--- flake.nix
+++ flake.nix
@@ -12,6 +12,7 @@
     nixosConfigurations = {
       default = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
+	specialArgs = {inherit inputs;};
         modules = [
           ./machines/x86_64-linux/configuration.nix
         ];
@@ -19,6 +20,7 @@
 
       darwin = nixpkgs.lib.nixosSystem {
         system = "aarch64-linux";
+	specialArgs = {inherit inputs;};
         modules = [
           ./machines/m1-vm/configuration.nix
         ];
