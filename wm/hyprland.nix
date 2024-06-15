fb0358c81a631619864c4acdf991061ab9467d25
Gibron Khan <khangibron@gmail.com>
2024-06-14 21:07:35 -0400

    use kitty in hyprland

--- wm/hyprland.nix
+++ wm/hyprland.nix
@@ -1,21 +1,24 @@
 { config, pkgs, ...}:
 
 {
+  home.packages = with pkgs; [
+    kitty
+  ];
+
   wayland.windowManager.hyprland.enable = true;
 
   wayland.windowManager.hyprland.settings = {
     "$mod" = "SUPER";
-    "$terminal" = "wezterm";
+    "$terminal" = "kitty";
 
     bind = [ 
       "$mod, t, exec, $terminal"
-      "$mod ALT, t, exec, kitty"
       "$mod, q, exit"
     ];
   };
 
   wayland.windowManager.hyprland.extraConfig = ''
-    exec-once = wezterm
+    exec-once = $terminal
   '';
 
   home.sessionVariables = {
