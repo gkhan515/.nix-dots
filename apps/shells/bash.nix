7b122c60528583476afe1697e42f9b1b2a732416
Gibron Khan <khangibron@gmail.com>
2024-08-01 21:46:49 -0400

    fix yt-dlp alias

--- apps/shells/bash.nix
+++ apps/shells/bash.nix
@@ -7,7 +7,7 @@
 
     shellAliases = {
       vim="nvim";
-      yt-dlp="yt-dlp -format='bestvideo[height<=?1080][fps<=?30][vcodec!=?vp9]+bestaudio/best'";
+      yt-dlp="yt-dlp -f \"bestvideo[height<=?1080][fps<=?30][vcodec!=vp9]+bestaudio\"";
       pf="peerflix --mpv --remove --list";
     };
   };
