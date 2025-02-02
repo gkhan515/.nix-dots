d588a65dc1921761ebd8d4f2cf5c9291d43d954b
Gibron Khan <khangibron@gmail.com>
2025-02-01 22:40:41 -0500

    lsp and cmp (no snippets)

--- apps/nixvim/default.nix
+++ apps/nixvim/default.nix
@@ -14,7 +14,40 @@
     };
 
     plugins = {
+      cmp = {
+        enable = true;
+        autoEnableSources = true;
+	settings = {
+          sources = [
+            { name = "buffer"; }
+            # { name = "luasnip"; }
+            { name = "nvim_lsp"; }
+            { name = "path"; }
+	  ];
+	  snippet.expand = ''
+            function (args)
+              require('luasnip').lsp_expand(args.body)
+	    end
+	  '';
+	};
+      };
+      # cmp-nvim-lsp.enable = true;
+      luasnip.enable = true;
+      # cmp_luasnip.enable = true;
+      # friendly-snippets.enable = true;
+
       indent-blankline.enable = true;
+
+      lsp = {
+        enable = true;
+	servers = {
+	  clangd.enable = true;    # C
+	  jdtls.enable = true;     # Java
+	  ts_ls.enable = true;     # Javascript/Typescript
+          lua_ls.enable = true;    # Lua
+	  pyright.enable = true;   # Python
+	};
+      };
 
       lualine.enable = true;
 
@@ -37,10 +70,10 @@
       };
     };
 
-    # extraPlugins = with pkgs.vimPlugins; [
-    #   # mini-base16 # needed for stylix
-    # ];
-    
+    extraPlugins = with pkgs.vimPlugins; [
+      # mini-base16 # needed for stylix
+    ];
+
     colorschemes.onedark.enable = true;
   };
 
